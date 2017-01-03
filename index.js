9c658afb662a87eb60c90f9f1e306657650cbc10
rogerbf <roger@bergfalk.nu>
2017-01-03 23:00:36 +0100

    formatting

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const { execFile } = require(`child_process`)
 
-module.exports = function hashPassword (password) {
-  return new Promise((resolve, reject) => {
-    execFile(`tor`, [ `--hash-password`, password ], (error, stdout, stderr) => {
-      error && reject(error)
-      stderr.length > 0 && reject(stderr.trim())
-      resolve(stdout.trim())
-    })
+module.exports = password => new Promise((resolve, reject) => {
+  execFile(`tor`, [ `--hash-password`, password ], (error, stdout, stderr) => {
+    error && reject(error)
+    stderr.length > 0 && reject(stderr.trim())
+    resolve(stdout.trim())
   })
-}
+})
