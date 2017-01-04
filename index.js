b5c75d20b29858b6ce76a2da25809bdfd6252dea
rogerbf <roger@bergfalk.nu>
2017-01-04 12:37:55 +0100

    accept optional tor path

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const { execFile } = require(`child_process`)
 
-module.exports = password => new Promise((resolve, reject) => {
-  execFile(`tor`, [ `--hash-password`, password ], (error, stdout, stderr) => {
-    error && reject(error)
-    stderr.length > 0 && reject(stderr.trim())
-    resolve(stdout.trim())
+module.exports = hashPassword
+
+function hashPassword (password, path = `tor`) {
+  return new Promise((resolve, reject) => {
+    execFile(path, [ `--hash-password`, password ], (error, stdout, stderr) => {
+      error && reject(error)
+      stderr.length > 0 && reject(stderr.trim())
+      resolve(stdout.trim())
+    })
   })
-})
+}
